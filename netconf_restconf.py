b55c3377900a5b20f9a8a86dbe52c2d856d4f8b0
gzapodea <gzapodea@cisco.com>
2018-06-06 14:58:52 -0700

    Ready fo CLUS 2018

---
+++ netconf_restconf.py
@@ -0,0 +1,73 @@
+
+
+# developed by Gabi Zapodeanu, TSA, Global Partner Organization
+
+
+import requests
+import ncclient
+import xml
+import xml.dom.minidom
+import urllib3
+import utils
+
+from ncclient import manager
+
+from urllib3.exceptions import InsecureRequestWarning  # for insecure https warnings
+from requests.auth import HTTPBasicAuth  # for Basic Auth
+
+urllib3.disable_warnings(InsecureRequestWarning)  # disable insecure https warnings
+
+SW_HOST = '10.93.130.21'
+RO_HOST = '10.93.130.41'
+PORT = 830
+USER ='cisco'
+PASS ='cisco'
+ROUTER_AUTH = HTTPBasicAuth(USER, PASS)
+
+
+def get_netconf_int_oper_status(interface):
+    """
+    This function will retrieve the IPv4 address configured on the interface via NETCONF
+    :param interface: interface name
+    :return: int_ip_add: the interface IPv4 address
+    """
+
+    with manager.connect(host=SW_HOST, port=PORT, username=USER,
+                         password=PASS, hostkey_verify=False,
+                         device_params={'name': 'default'},
+                         allow_agent=False, look_for_keys=False) as m:
+        # XML filter to issue with the get operation
+        # IOS-XE 16.6.2+        YANG model called "ietf-interfaces"
+
+        interface_state_filter = '''
+                                    <filter xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+                                        <interfaces-state xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+                                            <interface>
+                                                <name>''' + interface + '''</name>
+                                                <oper-status/>
+                                            </interface>
+                                        </interfaces-state>
+                                    </filter>
+                                '''
+
+        result = m.get(interface_state_filter)
+        xml_doc = xml.dom.minidom.parseString(result.xml)
+        int_info = xml_doc.getElementsByTagName('oper-status')
+        try:
+            oper_status = int_info[0].firstChild.nodeValue
+        except:
+            oper_status = 'unknown'
+        return oper_status
+
+
+def get_restconf_int_oper_status(interface):
+
+    url = 'https://' + RO_HOST + '/restconf/data/interfaces-state/interface=' + interface
+    header = {'Content-type': 'application/yang-data+json', 'accept': 'application/yang-data+json'}
+    response = requests.get(url, headers=header, verify=False, auth=ROUTER_AUTH)
+    interface_info = response.json()
+    oper_data = interface_info['ietf-interfaces:interface']
+    utils.pprint(oper_data)
+    oper_status = oper_data['oper-status']
+    return oper_status
+
