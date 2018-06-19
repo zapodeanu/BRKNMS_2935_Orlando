2ed8ce61dc96aa5240aa2cf6701c2f52e8a21706
Gabriel Zapodeanu <gzapodea@cisco.com>
2018-06-19 11:32:33 -0700

    Ready for CL US

---
+++ config.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+
+# developed by Gabi Zapodeanu, TSA, GPO, Cisco Systems
+
+# This file contains the Spark Auth, ServiceNow Dev account info, router info
+
+SPARK_URL = 'https://api.ciscospark.com/v1'
+SPARK_AUTH = 'Bearer ' + 'Your auth token'
+SPARK_ROOM = 'Config Change Alert'
+SPARK_MEMBER = 'accoutn email'
+
+PDX_RN = 'ip_address'
+
+SW_HOST = 'ip_address'
+RO_HOST = 'ip_address'
+PORT = 830
+USER ='cisco'
+PASS ='cisco'
+
+SNOW_URL = 'https://devxxxx.service-now.com/api/now'
+SNOW_ADMIN = 'admin_account'
+SNOW_DEV = 'ERNA'
+SNOW_PASS = 'password'
+SNOW_INSTANCE = 'devxxxx'
+
+
+TROPO_KEY = 'Your Tropo key'
+
+
+DNAC_URL = 'https://ip_address'
+DNAC_USER = 'admin'
+DNAC_PASS = 'password'
+
+
+ASAv_URL = 'https://ip_address'
+ASAv_USER = 'user'
+ASAv_PASSW = 'password'
+
+UCSD_URL = 'https://ip_address'
+UCSD_USER = 'user'
+UCSD_PASSW = 'password'
+UCSD_KEY = 'yourkey'
+
+APPROVER_EMAIL = 'email_address'
